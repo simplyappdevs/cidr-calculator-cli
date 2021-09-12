d2266cc95a9acd7dcf8e7ddf1bd261d82723d521
azmizar <azmizar.ujang@outlook.com>
2021-09-12 00:12:04 -0400

    update: adding debugger

--- src/cli.ts
+++ src/cli.ts
@@ -6,9 +6,20 @@
 /**
  * App Imports
  */
-import {default as execCLI} from './index';
+// import {Command, CommandImpl} from './command';
+// import {default as execCLI} from './index';
 
-// exec CLI
-(function () {
-  return execCLI(process.argv.slice(2));
-})();
+// // exec CLI
+// (function () {
+//   // const cmd: Command = new CommandImpl('cidr', 'Parse CIDR');
+
+//   // cmd.addArgument(['-c', '--cidr'], 'CIDR notation', 'N.N.N.N.N/CB', /.*/gi);
+//   // cmd.addArgument(['-i', '--ipv4'], 'IPv4 address', 'N.N.N.N', /.*/gi)
+//   //   .addArgument(['-cb', '--cidr-block'], 'CIDR block', 'CB', /.*/gi);
+
+//   // console.log(JSON.stringify(cmd));
+//   //return execCLI(process.argv.slice(2));
+//   console.log('hello');
+// })();
+
+console.log('test');
